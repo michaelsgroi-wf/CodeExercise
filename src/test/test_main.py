98e5780f1bd76481c2efe965c1eeb84492137c89
michaelsgroi-wf <michael.sgroi@workiva.com>
2018-05-03 16:24:17 -0500

    Update test_main.py

--- src/test/test_main.py
+++ src/test/test_main.py
@@ -9,13 +9,13 @@ import main
 class TestMain(unittest.TestCase):
 
     def test_asadfd(self):
-        self.assertEqual(main.execute('asadfd'), '3ed340ef38eb08086ea4ab6e0c1d171b')
+        self.assertEqual(main.execute('asadfd'), 'b2bcb52072cb6fc9ed64080a151d4f6e')
 
     def test_asaadfda(self):
-        self.assertEqual(main.execute('asaadfda'), '3ed340ef38eb08086ea4ab6e0c1d171b')
+        self.assertEqual(main.execute('asaadfda'), 'f9cd2e11ccff25c7f606c537ece64d7a')
 
     def test_asaadfdt(self):
-        self.assertEqual(main.execute('asaadfdt'), '3ed340ef38eb08086ea4ab6e0c1d171b')
+        self.assertEqual(main.execute('asaadfdt'), '27e54f02238a80ed60bf055f07ce37a9')
 
     def test_asadfdt(self):
         self.assertRaises(RuntimeError, main.execute('asadfdt'))
